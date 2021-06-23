82e7b8258347bbbfd6dc9ca76ef0351588468e92
Better1025 <924902324@qq.com>
2021-06-23 23:46:29 +0800

    workflow: 增加cypress自动化测试

--- scripts/utils/env.js
+++ scripts/utils/env.js
@@ -1,6 +1,10 @@
-const nodeEnv = process.env.NODE_ENV
-
-console.log('NODE_ENV', nodeEnv)
+const NODE_ENV = process.env.NODE_ENV
+console.log('NODE_ENV', NODE_ENV)
+if (!NODE_ENV) {
+  throw new Error(
+    'The NODE_ENV environment variable is required but was not specified.',
+  )
+}
 
 // webpack配置
 exports.webpackConfig = process.env.WEBPACK_CONFIG
@@ -8,5 +12,5 @@ exports.webpackConfig = process.env.WEBPACK_CONFIG
   : {}
 console.log('WEBPACK_CONFIG(webpack配置)', exports.webpackConfig)
 
-exports.isDev = nodeEnv === 'development'
-exports.isPro = nodeEnv === 'production'
+exports.isDev = NODE_ENV === 'development'
+exports.isPro = NODE_ENV === 'production'
